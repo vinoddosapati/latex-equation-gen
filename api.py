42a18ca7caf4a59ca409176453cfcaa33a4896e1
Vinod <vdosapati@albany.edu>
2021-05-10 15:34:32 -0400

    mobile add

--- api.py
+++ api.py
@@ -38,7 +38,33 @@ def getMathMlValue(equation):
     mathml_output = latex2mathml.converter.convert(finalVal)
     print("mathml out, ", mathml_output)
     return mathml_output
+    
+@app.route('/0.0.0.0:8080/imgdata/<data>', methods=['GET'])
+def apiclientCall1(data):
+    finalVal = data.replace("SLASH", "/")
+    print("got : ", data)
+    print("replaced", finalVal)
+    return expLatex(finalVal)
+
+@app.route('/0.0.0.0:8080/cal/<data>', methods=['GET'])
+def getCalValue1(data):
+    finalVal = data.replace("FSLASH", "\\")
+    finalVal = finalVal.replace("SLASH", "/")
+    print("--",finalVal,"---")
+    result = latexEval(finalVal)
+    print("res ", result)
+    return str(result)
+
+@app.route('/0.0.0.0:8080/mathml/<equation>', methods=['GET'])
+def getMathMlValue1(equation):
+    mathml_output = ""
+    print("---eq ", equation)
+    finalVal = equation.replace("FSLASH", "\\")
+    finalVal = finalVal.replace("SLASH", "/")
+    mathml_output = latex2mathml.converter.convert(finalVal)
+    print("mathml out, ", mathml_output)
+    return mathml_output
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
